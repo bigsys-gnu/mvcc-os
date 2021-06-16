dd8373e56af178cecd8c3ffb26d19ae40f15c361
MadPlayer <kch9001@gmail.com>
2021-06-16 17:56:21 +0900

    static cleaning ver 1

--- os/Xv6-kernel-threads/include/types.h
+++ os/Xv6-kernel-threads/include/types.h
@@ -7,6 +7,8 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+typedef long unsigned int size_t;
+typedef long intptr_t;
 #ifndef NULL
 #define NULL (0)
 #endif
