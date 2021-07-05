f740ded650f803fdaba66ace2d004397a8558b23
Jaeho <jaeho@vt.edu>
2021-07-05 13:49:22 +0900

    import-scalefs-fix-err: use scalefs' stat.h

--- os/scalefs/include/uk/stat.h
+++ os/scalefs/include/uk/stat.h
@@ -2,23 +2,13 @@
 #pragma once
 
 #include "fs.h"
-#include "time.h"
 
-struct kernel_stat {
-  dev_t st_dev;                 /* Device number */
-  ino_t st_ino;                 /* Inode number on device */
+struct stat {
+  mode_t  st_mode;              /* Mode (including file type) */
+  dev_t   st_dev;               /* Device number */
+  ino_t   st_ino;               /* Inode number on device */
   nlink_t st_nlink;             /* Number of links to file */
-  mode_t st_mode;               /* Mode (including file type) */
-  unsigned int st_uid;          /* User ID of the file's owner.        */
-  unsigned int st_gid;          /* Group ID of the file's group.*/
-  int __pad0;
-  dev_t st_rdev;                /* Device number, if device.  */
-  off_t st_size;                /* Size of file, in bytes.  */
-  long st_blksize;              /* Optimal block size for I/O.  */
-  long st_blocks;               /* Nr. 512-byte blocks allocated.  */
-  struct timespec st_atim;      /* Time of last access.  */
-  struct timespec st_mtim;      /* Time of last modification.  */
-  struct timespec st_ctim;      /* Time of last status change.  */
+  off_t   st_size;              /* Size of file in bytes */
 };
 
 #define S_IFMT 00170000
