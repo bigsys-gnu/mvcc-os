dc05de6d219c29725027453e996bae2ee6e6b5a2
MadPlayer <kch9001@gmail.com>
2021-08-11 21:07:04 +0900

    include cpu_relax

--- os/sv6/scalefs/include/mvrlu/port-user.h
+++ os/sv6/scalefs/include/mvrlu/port-user.h
@@ -3,6 +3,7 @@
 
 #include <sys/mman.h>
 #include "pthread.h"
+#include "mvrlu/arch.h"
 
 #define __init
 #define EXPORT_SYMBOL(sym)
@@ -145,7 +146,7 @@ static inline void port_free(void *ptr)
  * Synchronization
  */
 
-#define port_cpu_relax_and_yield() __asm__ volatile("rep; nop")
+#define port_cpu_relax_and_yield() cpu_relax
 
 static inline void port_spin_init(pthread_spinlock_t *lock)
 {
