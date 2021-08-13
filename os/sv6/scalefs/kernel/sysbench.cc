bab7e4bfd817170e3b94c7647280a0648153dd88
MadPlayer <kch9001@gmail.com>
2021-08-13 16:08:27 +0900

    implement spinlist

--- os/sv6/scalefs/kernel/sysbench.cc
+++ os/sv6/scalefs/kernel/sysbench.cc
@@ -19,6 +19,10 @@
 struct node {
   node *next;
   int value;
+
+  node(int val) : next(NULL), value(val) {}
+
+  NEW_DELETE_OPS(node);
 };
 
 template <typename T>
@@ -33,7 +37,7 @@ class list<spinlock> {
   node *head_;
   spinlock lk_;
 public:
-  list<spinlock>(void) : head_(NULL), lk_("spin bench") {}
+  list<spinlock>(void) : head_(new node(0)), lk_("spin bench") {}
   ~list<spinlock>(void) {
     for (auto iter = head_; iter != NULL;)
     {
@@ -44,19 +48,89 @@ public:
   }
 
   int list_insert(int key) {
-    return 0;
+    node *prev, *cur;
+    int ret = 0;
+
+    scoped_acquire(&lk_);
+    for (prev = head_, cur = prev->next; ; prev = cur, cur = cur->next)
+      {
+        if (cur == NULL || cur->value > key)
+          {
+            auto new_node = new node(key);
+            new_node->next = prev->next;
+            prev->next = new_node;
+            ret = 1;            // successed
+            break;
+          }
+        else if (cur->value == key)
+          break;                // the key value already exists.
+      }
+    return ret;
   }
 
   int list_delete(int key) {
-    return 0;
+    node *prev, *cur;
+    int ret = 0;
+
+    scoped_acquire(&lk_);
+    for (prev = head_, cur = prev->next; cur != NULL; prev = cur, cur = prev->next)
+      {
+        // found the target to be deleted
+        if (cur->value == key)
+          {
+            prev->next = cur->next;
+            delete cur;
+            ret = 1;
+            break;
+          }
+      }
+    return ret;
   }
 
   int list_find(int key) {
-    return 0;
+    node *cur;
+    int value = -1;
+
+    scoped_acquire(&lk_);
+    cur = head_;
+    while (cur && cur->value < key)
+      cur = cur->next;
+
+    // found the value
+    if (cur && cur->value == key)
+      value = cur->value;
+
+    return value;
   }
 
   int raw_insert(int key) {
-    return 0;
+    node *prev, *cur;
+    int ret = 0;
+
+    scoped_acquire(&lk_);
+    for (prev = head_, cur = prev->next; cur != NULL;
+         prev = cur, cur = cur->next)
+      {
+        if (key < cur->value)
+          {
+            ret = 1;
+            auto new_node = new node(key);
+            new_node->next = cur;
+            prev->next = new_node;
+            return ret;
+          }
+      }
+    else if(key == cur->value)
+      return ret;               // already exists
+
+    if (cur == NULL)
+      {
+        ret = 1;
+        auto new_node = new node(key);
+        new_node->next = cur;
+        prev->next = new_node;
+      }
+    return ret;
   }
   
   NEW_DELETE_OPS(list<spinlock>);
