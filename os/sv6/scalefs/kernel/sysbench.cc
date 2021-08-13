ab8d3274cf2bf1f1fceef5053868ca75065f300a
MadPlayer <kch9001@gmail.com>
2021-08-13 19:43:29 +0900

    add spinlock benchmark

--- os/sv6/scalefs/kernel/sysbench.cc
+++ os/sv6/scalefs/kernel/sysbench.cc
@@ -18,6 +18,14 @@
 
 #define HASH_VALUE(p_hash_list, val)       (val % p_hash_list.n_buckets)
 //////////////////////////////////////
+// BENCHMARK TYPES
+/////////////////////////////////////
+enum bench_type {
+  SPINLOCK
+};
+
+
+//////////////////////////////////////
 // RANDOM FUNCTIONS
 /////////////////////////////////////
 uint64_t
@@ -93,20 +101,19 @@ struct thread_param {
   int result_found;
   int &stop;
   unsigned short seed[3];
-  hash_list<T> &hash_list;
+  hash_list<T> &hl;
   thread_data<T> &data;
 
   thread_param(int n_buckets, int nb_threads, int update, int range,
-               int &stop, hash_list<T> &hash_list, thread_data<T> &data)
+               int &stop, hash_list<T> &hl, thread_data<T> &data)
     :n_buckets(n_buckets), nb_threads(nb_threads), update(update),
-     range(range), stop(stop), hash_list(hash_list), data(data), variation(0),
-     result_add(0), result_remove(0), result_contains(0), result_found(0) {
+     range(range), variation(0), result_add(0), result_remove(0),
+     result_contains(0), result_found(0), stop(stop), hl(hl), data(data) {
     rand_init(seed);
   }
 
   NEW_DELETE_OPS(thread_param<T>);
 };
-
 template <typename T>
 void test(void *param) {}
 
@@ -129,7 +136,7 @@ public:
     node *prev, *cur;
     int ret = 0;
 
-    scoped_acquire(&lk_);
+    scoped_acquire l(&lk_);
     for (prev = head_, cur = prev->next; ; prev = cur, cur = cur->next)
       {
         if (cur == NULL || cur->value > key)
@@ -150,7 +157,7 @@ public:
     node *prev, *cur;
     int ret = 0;
 
-    scoped_acquire(&lk_);
+    scoped_acquire l(&lk_);
     for (prev = head_, cur = prev->next; cur != NULL; prev = cur, cur = prev->next)
       {
         // found the target to be deleted
@@ -169,7 +176,7 @@ public:
     node *cur;
     int value = -1;
 
-    scoped_acquire(&lk_);
+    scoped_acquire l(&lk_);
     cur = head_;
     while (cur && cur->value < key)
       cur = cur->next;
@@ -185,7 +192,6 @@ public:
     node *prev, *cur;
     int ret = 0;
 
-    scoped_acquire(&lk_);
     for (prev = head_, cur = prev->next; cur != NULL;
          prev = cur, cur = cur->next)
       {
@@ -197,9 +203,9 @@ public:
             prev->next = new_node;
             return ret;
           }
+        else if(key == cur->value)
+          return ret;               // already exists
       }
-    else if(key == cur->value)
-      return ret;               // already exists
 
     if (cur == NULL)
       {
@@ -246,7 +252,7 @@ template <>
 void test<spinlock>(void *param) {
   int op, bucket, value;
   auto *p_data = reinterpret_cast<thread_param<spinlock> *>(param);
-  auto &hash_list = p_data->hash_list;
+  auto &hash_list = p_data->hl;
 
   cprintf("thread %d Start\n", myproc()->pid);
   // need condition for barrier
@@ -254,7 +260,7 @@ void test<spinlock>(void *param) {
     {
       op = rand_range(1000, p_data->seed);
       value = rand_range(p_data->range, p_data->seed);
-      bucket = HASH_VALUE(hash_list, value);
+      bucket = value % p_data->n_buckets;
       auto *p_list = hash_list.get_list(bucket);
 
       if (op < p_data->update)
@@ -286,4 +292,118 @@ void test<spinlock>(void *param) {
         }
     }
   cprintf("thread %d end\n", myproc()->pid);
+}
+
+void sleep_usec(u64 initial_time, u64 usec);
+
+template <typename T>
+void print_outcome(hash_list<T> &hl, thread_param<T> *param_list[], size_t len);
+
+//SYSCALL
+void
+sys_benchmark(int nb_threads, int initial, int n_buckets, int duration, int update,
+              int range)
+{
+  enum bench_type type = SPINLOCK;
+  cprintf("Run Kernel Level Benchmark\n");
+
+  assert(n_buckets >= 1);
+  assert(duration >= 0);
+  assert(initial >= 0);
+  assert(nb_threads > 0);
+  assert(update >= 0 && update <= 1000);
+  assert(range > 0 && range >= initial);
+
+  if (type == SPINLOCK)
+    {
+      hash_list<spinlock> *hl = new hash_list<spinlock>(n_buckets);
+
+      cprintf("initialize %d nodes...", initial);
+      int i = 0;
+      unsigned short seed[3];
+
+      rand_init(seed);
+      while (i < initial)
+        {
+          int value = rand_range(range, seed);
+          auto list = hl->get_list(value % n_buckets);
+
+          if (list->raw_insert(value))
+            i++;
+        }
+      cprintf("done\n");
+
+      // allocate pointer list
+      struct proc **thread_list = new struct proc *[nb_threads];
+      thread_param<spinlock> **param_list = new thread_param<spinlock> *[nb_threads];
+
+      // mile sec
+      u64 initial_time = nsectime();
+      cprintf("Main thread ID: %d\n", myproc()->pid);
+      cprintf("Creating %d threads...", nb_threads);
+      int stop = 0;             // shared by threads
+      thread_data<spinlock> d;  // spinlock need no data. this is just shell.
+      for (int i = 0; i < nb_threads; i++)
+        {
+          param_list[i] = new thread_param<spinlock>(n_buckets, nb_threads, update,
+                                                     range, stop, *hl, d);
+        }
+      for (int i = 0; i < nb_threads; i++)
+        {
+        thread_list[i] = threadpin(test<spinlock>, (void*)param_list[i], "test_thread",
+                                   i%(NCPU-1)+1);
+        cprintf("\nThread created %p(c:%d, s:%d)\n", thread_list[i], i%(NCPU-1)+1,
+                thread_list[i]->get_state());
+        }
+      cprintf(" done!\n");
+
+      sleep_usec(initial_time, duration);
+
+      stop = 1;
+      cprintf("join %d threads...\n", nb_threads);
+      for(int i = 0; i < nb_threads; i++)
+        wait(-1, NULL);
+
+      cprintf(" done!\n");
+
+      print_outcome<spinlock>(*hl, param_list, nb_threads);
+
+      // deallocate memory
+      delete hl;
+      for (int j = 0; j < nb_threads; j++)
+        {
+          delete param_list[j];
+        }
+      delete[] thread_list;
+      delete[] param_list;
+    } // spinlock
+  
+}
+
+// initial_time is nano sec
+void sleep_usec(u64 initial_time, u64 usec) {
+  spinlock l("l");
+  condvar cond("sleep cond");
+  u64 until = usec * 1000000 + initial_time;
+
+  scoped_acquire local(&l);
+  if (until > nsectime())
+    cond.sleep_to(&l, until);
+}
+
+template <typename T>
+void print_outcome(hash_list<T> &hl, thread_param<T> *param_list[], size_t len) {
+  int reads, updates, total_variation;
+
+  for (int i = 0; i < len; i++)
+    {
+      cprintf( "Thread %d\n", i);
+      cprintf( "  #add        : %d\n", param_list[i]->result_add);
+      cprintf( "  #remove     : %d\n", param_list[i]->result_remove);
+      cprintf( "  #contains   : %d\n", param_list[i]->result_contains);
+      cprintf( "  #found      : %d\n", param_list[i]->result_found);
+      reads += param_list[i]->result_found;
+      updates += (param_list[i]->result_add + param_list[i]->result_remove);
+      total_variation += param_list[i]->variation;
+    }
 }
