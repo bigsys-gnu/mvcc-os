beb829b0f9708564bec3d378573b2ff019a85f99
MadPlayer <kch9001@gmail.com>
2021-08-16 11:56:07 +0900

    add mvrlu cpp header

---
+++ os/sv6/scalefs/include/mvrlu/mvrlu.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "mvrlu.h"
+#include "mvrlu_i.h"
+#include <cstddef>
+
+namespace mvrlu {
+  template <typename T> class thread_handle;
+  template <typename T> class mvrlu_object;
+
+  // call RLU_INIT, RLU_FINISH manually
+  // this mvrlu can be applied only one data structure.
+  template <typename T>
+  inline void
+  mvrlu_assign_pointer(T **p_ptr, T *obj) {
+    ::_mvrlu_assign_pointer((void **)p_ptr, (void *)obj);
+  }
+
+  template <typename T>
+  inline bool
+  mvrlu_cmp_ptrs(T *obj, T *obj2) {
+    return ::mvrlu_cmp_ptrs((void *)obj, (void *)obj2);
+  }
+
+  template <typename T>
+  inline T *
+  mvrlu_alloc(std::size_t size) {
+    return (T *) ::mvrlu_alloc(size);
+  }
+
+  template <typename T>
+  class thread_handle {
+  public:
+    thread_handle(void) {
+      ::mvrlu_thread_init(&self_);
+    }
+
+    ~thread_handle(void) {
+      ::mvrlu_thread_finish(&self_);
+    }
+
+    // don't deallocate memory right away
+    // _qp_thread will take care of.
+    void operator delete(void *, std::size_t) {}
+
+    inline void
+    mvrlu_reader_lock(void) {
+      ::mvrlu_reader_lock(&self_);
+    }
+
+    inline void
+    mvrlu_reader_unlock(void) {
+      ::mvrlu_reader_unlock(&self_);
+    }
+
+    inline bool
+    mvrlu_try_lock(T** p_p_obj) {
+      return ::_mvrlu_try_lock(&self_, (void **)p_p_obj, sizeof(T));
+    }
+
+    inline bool
+    mvrlu_try_lock_const(T* obj) {
+      return ::_mvrlu_try_lock_const(&self_, (void *)obj, sizeof(T));
+    }
+
+    inline void
+    mvrlu_abort(void) {
+      ::mvrlu_abort(&self_);
+    }
+
+    inline T*
+    mvrlu_deref(T *p_obj) {
+      return (T *) ::mvrlu_deref(&self_, (void *)p_obj);
+    }
+
+    // need hotfix!!!
+    // - how to call deleter of p_obj properly
+    // 1. mvrlu.c don't know p_obj is which type.
+    // -> mvrlu.c need to be fixed using template?
+    inline void
+    mvrlu_free(T *p_obj) {
+      ::mvrlu_free(&self_, (void *)p_obj);
+    }
+
+  private:
+    mvrlu_thread_struct_t self_;
+    friend class mvrlu_object<T>;
+  };
+
+  template <typename T>
+  class mvrlu_object {
+    T *ptr_;
+    thread_handle<T> &handle_;
+  public:
+    mvrlu_object(T *ptr, thread_handle<T> &handle): ptr_(ptr), handle_(handle) {}
+    ~mvrlu_object(void) {
+      // in mvrlu.c, ptr_'s deleter will not be called!
+      handle_.mvrlu_free(ptr_);
+    }
+  };
+
+}
