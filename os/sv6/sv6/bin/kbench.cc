5f003868b4bd79f1970a875abb9439b52cc1317e
MadPlayer <kch9001@gmail.com>
2021-10-04 12:40:56 +0900

    make kernel benchmark outcome visible on user level

--- os/sv6/sv6/bin/kbench.cc
+++ os/sv6/sv6/bin/kbench.cc
@@ -2,6 +2,7 @@
 #include "user.h"
 #include "amd64.h"
 #include "lib.h"
+#include "mvcc_kernel_bench.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +18,8 @@
 #define DEFAULT_NB_THREADS              4
 #define DEFAULT_RANGE                   (DEFAULT_INITIAL * 2)
 #define HASH_VALUE(p_hash_list, val)       (val % p_hash_list->n_buckets)
+#define PRINT_BENCH(sync_type) printf("************ %s ***************\n", \
+                                      type_names[sync_type])
 
 void
 usage(const char *argv0)
@@ -28,23 +31,35 @@ usage(const char *argv0)
   fprintf(stderr, "  -d micro seconds of benchmark\n");
   fprintf(stderr, "  -u update ratio (20 is 2%%)\n");
   fprintf(stderr, "  -s sync type\n");
+  fprintf(stderr, "  -r range\n");
   exit(2);
 }
 
-#define SPINLOCK 0
-#define MVRLU 1
+enum {
+  SPINLOCK, MVRLU, RCU
+};
+
+const char *type_names[] = {
+  "SPINLOCK",
+  "MVRLU",
+  "RCU",
+};
 
 int
 main(int argc, char **argv)
 {
+  kernel_bench_args arguments = {
+    .nb_threads = DEFAULT_NB_THREADS,
+    .initial = DEFAULT_INITIAL,
+    .n_buckets = DEFAULT_BUCKETS,
+    .duration = DEFAULT_DURATION,
+    .update = DEFAULT_UPDATE,
+    .range = DEFAULT_RANGE,
+    .sync_type = SPINLOCK,
+  };
+  kernel_bench_outcome outcome;
+
   printf("kernel lavel benchmark start\n");
-  int n_buckets = DEFAULT_BUCKETS;
-  int initial = DEFAULT_INITIAL;
-  int nb_threads = DEFAULT_NB_THREADS;
-  int duration = DEFAULT_DURATION;
-  int update = DEFAULT_UPDATE;
-  int type = SPINLOCK;
-  int range = DEFAULT_RANGE;
 
   int opt;
   while ((opt = getopt(argc, argv, "b:i:t:d:u:s:r:")) != -1)
@@ -52,55 +67,68 @@ main(int argc, char **argv)
     switch (opt)
     {
     case 's':
-      type = atoi(optarg);
+      arguments.sync_type = atoi(optarg);
       break;
     case 'u':
-      update = atoi(optarg);
+      arguments.update = atoi(optarg);
       break;
     case 'd':
-      duration = atoi(optarg);
+      arguments.duration = atoi(optarg);
       break;
     case 't':
-      nb_threads = atoi(optarg);
+      arguments.nb_threads = atoi(optarg);
       break;
     case 'i':
-      initial = atoi(optarg);
+      arguments.initial = atoi(optarg);
       break;
     case 'b':
-      n_buckets = atoi(optarg);
+      arguments.n_buckets = atoi(optarg);
       break;
     case 'r':
-      range = atoi(optarg);
+      arguments.range = atoi(optarg);
       break;
     default:
       usage(argv[0]);
     }
   }
 
-  assert(n_buckets >= 1);
-  assert(duration >= 0);
-  assert(initial >= 0);
-  assert(nb_threads > 0);
-  assert(update >= 0 && update <= 1000);
-  // assert(range > 0 && range >= initial);
-  // assert(n_buckets < range);
+  assert(arguments.n_buckets >= 1);
+  assert(arguments.duration >= 0);
+  assert(arguments.initial >= 0);
+  assert(arguments.nb_threads > 0);
+  assert(arguments.update >= 0 && arguments.update <= 1000);
+  assert(arguments.range > 0 && arguments.range >= arguments.initial);
+  assert(arguments.n_buckets < arguments.range);
 
-  printf("-t #threads     : %d\n", nb_threads);
-  printf("-i Initial size : %d\n", initial);
-  printf("-b Buckets      : %d\n", n_buckets);
-  printf("-d Duration     : %d\n", duration);
-  printf("-u Update rate  : %d\n", update);
-  printf("-s sync type    : %d\n", type);
+  printf("-t #threads     : %d\n", arguments.nb_threads);
+  printf("-i Initial size : %d\n", arguments.initial);
+  printf("-b Buckets      : %d\n", arguments.n_buckets);
+  printf("-d Duration     : %d\n", arguments.duration);
+  printf("-u Update rate  : %d\n", arguments.update);
+  printf("-s sync type    : %d\n", arguments.sync_type);
+  printf("-r Range        : %d\n", arguments.range);
   printf("Benchmark type  : hash-list\n");
 
 
-  benchmark(
-    nb_threads,
-    initial,
-    n_buckets,
-    duration,
-    update,
-    type);
+  benchmark((void *)&arguments, (void *)&outcome);
+
+  printf( "\n#### B ####\n");
+
+  printf( "Set size      : %lu (expected: %lu)\n", outcome.total_size, outcome.exp);
+  printf( "Duration      : %d (ms)\n", arguments.duration);
+  unsigned long iv = outcome.total_read * 1000.0 / arguments.duration;
+  unsigned long fv = (unsigned long)(outcome.total_read * 1000.0 / arguments.duration * 10) % 10;
+  printf( "#read ops     : %lu (%lu.%lu / s)\n", outcome.total_read, iv, fv);
+  iv = outcome.total_update * 1000.0 / arguments.duration;
+  fv = (unsigned long)(outcome.total_update * 1000.0 / arguments.duration * 10) % 10;
+  printf( "#update ops   : %lu (%lu.%lu / s)\n", outcome.total_update, iv, fv);
+
+  if(outcome.exp != outcome.total_size)
+  {
+    printf("\n<<<<<< ASSERT FAILURE(%lu!=%lu) <<<<<<<<\n", outcome.exp,
+           outcome.total_size);
+  }
+  PRINT_BENCH(arguments.sync_type);
 
   return 1;
 }
