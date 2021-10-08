f572b733b54036f5834f3f3e022a9d6c69399ac4
MadPlayer <kch9001@gmail.com>
2021-10-09 04:13:48 +0900

    first version of mvrlu chainhash

--- os/sv6/scalefs/include/mvrlu/chainhash.hh
+++ os/sv6/scalefs/include/mvrlu/chainhash.hh
@@ -4,12 +4,11 @@
  * A bucket-chaining hash table.
  */
 
-#include "spinlock.hh"
-#include "lockwrap.hh"
 #include "hash.hh"
-#include "ilist.hh"
 #include "hpet.hh"
 #include "cpuid.hh"
+#include "mvrlu/mvrlu.hh"
+#include "mvrlu/list.hh"
 
 /*
  * MV-RLU manages only pure value
@@ -19,28 +18,31 @@ namespace mvrlu {
   template<class K, class V>
   class chainhash {
   private:
-    struct item : public rcu_freed {
+    struct item {
       item(const K& k, const V& v)
-        : rcu_freed("chainhash::item", this, sizeof(*this)),
-          key(k), val(v) {}
-      void do_gc() override { delete this; }
-      NEW_DELETE_OPS(item);
+        : key(k), val(v) {}
 
-      islink<item> link;  //ilist.hh -> singly-linked lists
+      MVRLU_NEW_DELETE(item);
+
+      link<item> link;
       const K key;
       V val;
     };
 
     struct bucket {
-      spinlock lock __mpalign__;
-      islist<item, &item::link> chain; //ilist.hh -> intrusive singly-linked list
+      list<item, &item::link> chain;
 
       ~bucket() {
-        while (!chain.empty()) {
-          item *i = &chain.front();
-          chain.pop_front();
-          gc_delayed(i);  //gc.hh 
+        auto &h = *myproc()->handle;
+
+        // need fix here
+        mvrlu_section s;
+        for (auto it = chain.begin(); it != chain.end();) {
+          auto trash = it;
+          it++;
+          h.mvrlu_free(&*trash);
         }
+        h.mvrlu_free(&*chain.before_begin());
       }
     };
 
@@ -60,14 +62,21 @@ namespace mvrlu {
     NEW_DELETE_OPS(chainhash);
 
     bool insert(const K& k, const V& v, u64 *tsc = NULL) {
+      auto &h = *myproc()->handle;
       bucket* b = &buckets_[hash(k) % nbuckets_];
-      scoped_acquire l(&b->lock);
 
+    restart:
+      mvrlu_section s;
       for (const item& i: b->chain)
         if (i.key == k)
           return false;
 
-      b->chain.push_front(new item(k, v));
+      auto head = b->chain.before_begin();
+      if (!head.try_lock())
+        goto restart;
+
+      b->chain.insert_after(head, new item(k, v));
+
       if (tsc)
         *tsc = get_tsc();
       return true;
@@ -77,19 +86,26 @@ namespace mvrlu {
       if (!lookup(k))
         return false;
 
-      bucket* b = &buckets_[hash(k) % nbuckets_];
-      scoped_acquire l(&b->lock);
+      auto &h = *myproc()->handle;
+      bucket *b = &buckets_[hash(k) % nbuckets_];
 
+    restart:
+      mvrlu_section s;
       auto i = b->chain.before_begin();
       auto end = b->chain.end();
-      for (;;) {
+      for (;;)
+      {
         auto prev = i;
         ++i;
         if (i == end)
           return false;
-        if (i->key == k && i->val == v) {
+        if (i->key == k && i->val == v)
+        {
+          if (!prev.try_lock() || !i.try_lock_const())
+            goto restart;
+
           b->chain.erase_after(prev);
-          gc_delayed(&*i);
+          h.mvrlu_free(&*i);
           if (tsc)
             *tsc = get_tsc();
           return true;
@@ -101,19 +117,26 @@ namespace mvrlu {
       if (!lookup(k))
         return false;
 
+      auto &h = *myproc()->handle;
       bucket* b = &buckets_[hash(k) % nbuckets_];
-      scoped_acquire l(&b->lock);
 
+    restart:
+      mvrlu_section s;
       auto i = b->chain.before_begin();
       auto end = b->chain.end();
-      for (;;) {
+      for (;;)
+      {
         auto prev = i;
         ++i;
         if (i == end)
           return false;
-        if (i->key == k) {
+        if (i->key == k)
+        {
+          if (!prev.try_lock() || !i.try_lock_const())
+            goto restart;
+
           b->chain.erase_after(prev);
-          gc_delayed(&*i);
+          h.mvrlu_free(&*i);
           if (tsc)
             *tsc = get_tsc();
           return true;
@@ -122,7 +145,7 @@ namespace mvrlu {
     }
 
     bool enumerate(const K* prev, K* out) const {
-      scoped_gc_epoch rcu_read;
+      mvrlu_section s;
 
       bool prevbucket = (prev != nullptr);
       for (u64 i = prev ? hash(*prev) % nbuckets_ : 0; i < nbuckets_; i++) {
@@ -144,7 +167,7 @@ namespace mvrlu {
 
     template<class CB>
     void enumerate(CB cb) const {
-      scoped_gc_epoch rcu_read;
+      mvrlu_section s;
 
       for (u64 i = 0; i < nbuckets_; i++) {
         bucket* b = &buckets_[i];
@@ -157,7 +180,7 @@ namespace mvrlu {
     }
 
     int getSize() {
-      scoped_gc_epoch rcu_read;
+      mvrlu_section s;
 
       int size = 0;
 
@@ -179,10 +202,11 @@ namespace mvrlu {
     }
 
     bool lookup(const K& k, V* vptr = nullptr) const {
-      scoped_gc_epoch rcu_read;
+      bucket *b = &buckets_[hash(k) % nbuckets_];
 
-      bucket* b = &buckets_[hash(k) % nbuckets_];
-      for (const item& i: b->chain) {
+      mvrlu_section s;
+      for (const item& i : b->chain)
+      {
         if (i.key != k)
           continue;
         if (vptr)
