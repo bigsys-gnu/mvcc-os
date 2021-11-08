e34f510c549869a96e1f0a0684325893f38c5b15
MadPlayer <kch9001@gmail.com>
2021-11-08 15:28:46 +0900

    use spinlock allocator

--- os/sv6/scalefs/include/mvrlu/log_allocator.hh
+++ os/sv6/scalefs/include/mvrlu/log_allocator.hh
@@ -36,37 +36,11 @@ namespace mvrlu {
     size_t end_addr_;
 
   public:
-    log_allocator() {
-      auto log_chunks = new log_space;
-      for (log_block &b : log_chunks->space)
-        log_pool_.push_front(&b);
+    log_allocator();
 
-      start_addr_ = reinterpret_cast<size_t>(log_chunks);
-      end_addr_ = start_addr_ + sizeof(log_block) * MVRLU_MAX_THREAD_NUM;
-    }
+    void * alloc_log_mem();
 
-    void *
-    alloc_log_mem() {
-      scoped_acquire l(&pool_lock_);
-
-      if (!log_pool_.empty())
-      {
-        auto& block = log_pool_.front();
-        log_pool_.pop_front();
-        return block.log_part;
-      }
-      return nullptr;
-    }
-
-    void
-    free_log_mem(void *log_part) {
-      auto *b = log_block::cast_to_block(log_part);
-      if (addr_in_log_region(b))
-      {
-        scoped_acquire l(&pool_lock_);
-        log_pool_.push_front(b);
-      }
-    }
+    void free_log_mem(void *log_part);
 
     bool
     addr_in_log_region(void *addr) {
